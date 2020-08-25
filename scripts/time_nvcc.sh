dacbcd6339a24ac25c106a42ffbc3f334e44ada7
Ryuichi Sai <ryuichi@rice.edu>
2020-08-24 22:54:38 -0500

    Time measurement for grid size of 893

--- scripts/time_nvcc.sh
+++ scripts/time_nvcc.sh
@@ -13,9 +13,22 @@ cd ..
 echo "## Basic Time Measurements" > $REPORT
 echo "" >> $REPORT
 
-NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_8x8x8_opt-gpu             "CUDA Global Memory (3D 8x8x8 Blocking)"                            nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 2 --warm-up $*"
-NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_gmem_fastmath_opt-gpu             "CUDA Global Memory (3D 8x8x8 Blocking)"                            nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 2 --warm-up $*"
-NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_reorder_opt-gpu             "CUDA Global Memory (3D 8x8x8 Blocking)"                            nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 2 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_8x8x8_s_opt-gpu             "CUDA Global Memory [Single-Stream] (Original)"                             nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_gmem_fastmath_s_opt-gpu          "CUDA Global Memory [Single-Stream] (Fast Math)"                            nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_reorder_s_opt-gpu           "CUDA Global Memory [Single-Stream] (Reorder)"                              nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_gmem_both_s_opt-gpu              "CUDA Global Memory [Single-Stream] (Both)"                                 nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
 
-#NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_smem_u_opt-gpu                 "CUDA Shared Memory on U"                                           nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 2 --warm-up $*"
-#NVCCFLAGS="-arch=sm_70 -maxrregcount=64"    time_run    cuda_matsu25d_32x32_opt-gpu         "CUDA Kazuaki Matsumuta (2.5D 32x32)"                               nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 2 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_8x8x8_opt-gpu               "CUDA Global Memory [Multi-Stream] (Original)"                              nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_gmem_fastmath_opt-gpu            "CUDA Global Memory [Multi-Stream] (Fast Math)"                             nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_gmem_reorder_opt-gpu             "CUDA Global Memory [Multi-Stream] (Reorder)"                               nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_gmem_both_opt-gpu                "CUDA Global Memory [Multi-Stream] (Both)"                                  nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_smem_u_s_opt-gpu                 "CUDA Shared Memory [Single-Stream] (Original)"                             nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_smem_u_fastmath_s_opt-gpu        "CUDA Shared Memory [Single-Stream] (Fast Math)"                            nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_smem_u_reorder_s_opt-gpu         "CUDA Shared Memory [Single-Stream] (Reorder)"                              nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_smem_u_both_s_opt-gpu            "CUDA Shared Memory [Single-Stream] (Both)"                                 nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_smem_u_opt-gpu                   "CUDA Shared Memory [Multi-Stream] (Original)"                              nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_smem_u_fastmath_opt-gpu          "CUDA Shared Memory [Multi-Stream] (Fast Math)"                             nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70                 "    time_run    cuda_smem_u_reorder_opt-gpu           "CUDA Shared Memory [Multi-Stream] (Reorder)"                               nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
+NVCCFLAGS="-arch=sm_70 --use_fast_math "    time_run    cuda_smem_u_both_opt-gpu              "CUDA Shared Memory [Multi-Stream] (Both)"                                  nvcc    "$COMPILER_NAME"    $REPORT     "--grid 893 --niters 5 --warm-up $*"
